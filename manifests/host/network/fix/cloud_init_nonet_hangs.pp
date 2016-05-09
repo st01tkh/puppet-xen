7f6167efdbc0e2330f0e35d5d02d70927fcb16bb
st01tkh <st01tkh@gmail.com>
2016-05-09 15:42:49 +0300

    Added reduce dowait in cloud-init-nonet.conf. This is bad solution because updates can rewrite it later to default

--- manifests/host/network/fix/cloud_init_nonet_hangs.pp
+++ manifests/host/network/fix/cloud_init_nonet_hangs.pp
@@ -3,9 +3,16 @@ class xen::host::network::fix::cloud_init_nonet_hangs {
     file {'place_cloud_init_nonet_hangs_fix_sh':
       source => 'puppet:///modules/xen/cloud_init_nonet_hangs_fix.sh',
       path => '/etc/network/cloud_init_nonet_hangs_fix.sh',
+      owner => 'root',
+      group => 'root',
+      mode => '0755',
     } ->
     exec {'add_to_rc_local_place_cloud_init_nonet_hangs_fix_sh':
       path => ['/bin', '/sbin', '/usr/bin', '/usr/sbin', '/usr/local/bin', '/usr/local/sbin'],
       command => "grep -q -F 'cloud_init_nonet_hangs_fix.sh' /etc/rc.local || sed -i -r '/^exit 0/i /etc/network/cloud_init_nonet_hangs_fix.sh' /etc/rc.local"
+    }
+    exec {'reduce_dowait_in_cloud_init_nonet_conf':
+      path => ['/bin', '/sbin', '/usr/bin', '/usr/sbin', '/usr/local/bin', '/usr/local/sbin'],
+      command => "sed -i 's/dowait 120/dowait 2/' /etc/init/cloud-init-nonet.conf; sed -i 's/dowait 10/dowait 1/' /etc/init/cloud-init-nonet.conf"
     }
 }
