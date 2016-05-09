d11de0277a02b67bac88b59db2fa684bbaf85978
st01tkh <st01tkh@gmail.com>
2016-05-09 04:12:36 +0300

    Added prevention of multiple replacement of ethX to xenbrX

--- files/functions.bash
+++ files/functions.bash
@@ -1,5 +1,36 @@
 #! /usr/bin/env bash
 
+function replace_eth_to_xenbr_in_file() {
+  local path; path="$1"
+  grep -q -F "iface xenbr" "$path" || sed -i -r 's/(iface|auto) eth/\1 xenbr/g' "$path" 
+}
+
+function replace_eth_to_xenbr_in_interfaces() {
+  replace_eth_to_xenbr_in_file '/etc/network/interfaces'
+}
+
+function replace_eth_to_xenbr_in_dir() {
+  local dir; dir="$1"
+  local find_cmd; find_cmd="find '$dir' -mindepth 1 -maxdepth 1 -type f"
+  [ -n "$2" ] && find_cmd="$find_cmd -name '$2'"
+  [ ! -d "$dir" ] && echo "Error: no such dir '$dir' or it's not a dir" && return 1
+  local f; eval "$find_cmd" | while read f; do
+    replace_eth_to_xenbr_in_file "$f"
+  done
+}
+
+function replace_eth_to_xenbr_in_interfaces_d() {
+  replace_eth_to_xenbr_in_dir '/etc/network/interfaces.d' '*.cfg'
+}
+
+function add_auto_manual_to_each_xenbr_in_interfaces() {
+  add_auto_manual_to_each_xenbr '/etc/network/interfaces'
+}
+
+function add_auto_manual_to_each_xenbr_in_interfaces_d() {
+  add_auto_manual_to_each_xenbr_in_dir '/etc/network/interfaces.d' '*.cfg'
+}
+
 function add_auto_manual_to_each_xenbr() {
   local path; path="$1"
   [ ! -e "$path" ] && echo "Error: no such file '$path' or it's not a dir" && return 1
@@ -24,6 +55,7 @@ function add_bridge_ports_to_each_xenbr() {
   grep -R -v '^#' "$path" | sed -nr 's/^\W*iface\W+(xenbr[0-9]+).*$/\1/p' | sort | uniq \
     | while read line; do
       ifnum="$(echo "$line" | sed -nr 's/^.*([0-9]+)$/\1/p')"
+      echo "ifport: $ifport"
       ifport="eth$ifnum"
       grep "iface $line" -A 1 "$path" | grep -q -F "bridge_ports $ifport" || sed -i -r "/iface $line/a \ \ \ \ bridge_ports $ifport" "$path" 
       #sed -i -r "/iface $line/a \ \ \ \ bridge_ports $ifport" "$path" 
@@ -72,6 +104,21 @@ self_fpath="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/$(basename "${BASH_SOU
 exec_fpath="$(cd "$(dirname "$0")" && pwd)/$(basename "$0")"
 if [ "$self_fpath" = "$exec_fpath" ]; then
   case "$1" in
+    rexf)
+      replace_eth_to_xenbr_in_file $2
+      ;;
+    rexd)
+      replace_eth_to_xenbr_in_dir $2 $3
+      ;;
+    rexif)
+      replace_eth_to_xenbr_in_interfaces
+      ;;
+    rexid)
+      replace_eth_to_xenbr_in_interfaces_d
+      ;;
+    rexifd)
+      replace_eth_to_xenbr_in_interfaces && replace_eth_to_xenbr_in_interfaces_d
+      ;;
     aamf)
       add_auto_manual_to_each_xenbr $2
       ;;
@@ -108,6 +155,12 @@ if [ "$self_fpath" = "$exec_fpath" ]; then
       ;;
     *)
       echo "Usage: $0 <aamf|aamd>"
+      echo "  rexf - Replace eth to xenbr in file <FILEPATH>"
+      echo "  rexd - Replace eth to xenbr in dir <DIRPATH> file by mask <MASK>"
+      echo "  rexif - Replace eth to xenbr in /etc/network/interfaces"
+      echo "  rexid - Replace eth to xenbr in /etc/network/interfaces/*cfg"
+      echo "  rexifd - rexif and rexid"
+      echo ""
       echo "  aamf - Add auto/manual to file <FILEPATH>"
       echo "  aamd - Add auto/manual to each file in dir <DIRPATH> in file by mask <MASK>"
       echo "  aamif - Add auto/manual to /etc/network/intefaces"
