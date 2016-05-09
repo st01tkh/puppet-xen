e527c1499083a55a6e16e6708e4714d5d75b69ef
st01tkh <st01tkh@gmail.com>
2016-05-09 16:31:39 +0300

    Added xen_tools to common. Switched packages default to common.

--- manifests/host/packages.pp
+++ manifests/host/packages.pp
@@ -1,13 +1,13 @@
-class xen::host::packages($set = 'minimal', $networking_type = 'bridge') {
+class xen::host::packages($set = 'common', $networking_type = 'bridge') {
   case $set {
     'minimal': {
-	notify {'installing minimal': }
-	include xen::host::packages::minimal
+	  notify {'installing minimal': }
+	  include xen::host::packages::minimal
     }
     'additional': { include xen::host::packages::additional }
     'common': {
       include xen::host::packages::minimal
-      include xen::host::packages::additional
+      include xen::host::packages::xen_tools
     }
   } 
   case $networking_type {
